f201f9f7cfccc5312968167bfe34a0a433eeb7fb
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-04-08 21:11:22 -0300

    fase 3

--- components/BottonTabNavigator.js
+++ components/BottonTabNavigator.js
@@ -1,6 +1,8 @@
 import React,{Component} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons"
+
 import TransactionScreen from "../screens/TransactionScreen";
 import SearchScreen from "../screens/SearchScreen";
 
@@ -10,7 +12,51 @@ export default class BottomTabNavigator extends Component{
     render(){
         return(
             <NavigationContainer>
-                <Tab.Navigator>
+                <Tab.Navigator
+                    screenOptions={({route}) =>({
+                        tabBarIcon:({focused,color,size})=>{
+                            var iconName
+                            if (route.name==="transação") {
+                                iconName="book"
+                            }
+                            else if(route.name==="pesquisa"){
+                                iconName="search"
+                            }
+                            return(
+                                <Ionicons name={iconName} size={size} color={color}  />
+
+                                
+                            )
+
+                        }
+
+                    })} 
+                    tabBarOptions={{
+                        activeTintColor: "#FFFFFF",
+                        inactiveTintColor: "black",
+                        style: {
+                          height: 130,
+                          borderTopWidth: 0,
+                          backgroundColor: "#5653d4"
+                        },
+                        labelStyle: {
+                          fontSize: 20,
+                          fontFamily: "Rajdhani_600SemiBold"
+                        },
+                        labelPosition: "beside-icon",
+                        tabStyle: {
+                          marginTop: 25,
+                          marginLeft: 10,
+                          marginRight: 10,
+                          borderRadius: 30,
+                          borderWidth: 2,
+                          alignItems: "center",
+                          justifyContent: "center",
+                          backgroundColor: "#5653d4"
+                        }
+                      }}
+                    >
+
                     <Tab.Screen name="transação" component={TransactionScreen} />
 
                     <Tab.Screen name="pesquisa" component={SearchScreen} />
